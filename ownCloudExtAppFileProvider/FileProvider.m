bd89c649b7625f3af9de3376452b917f11ec88be
Gonzalo Gonzalez <ggonzalez@apperian.com>
2015-03-04 08:51:53 +0100

    Change the headers of the files

--- ownCloudExtAppFileProvider/FileProvider.m
+++ ownCloudExtAppFileProvider/FileProvider.m
@@ -5,12 +5,25 @@
 //  Created by Gonzalo Gonzalez on 14/10/14.
 //
 
-/*
- Copyright (C) 2014, ownCloud, Inc.
- This code is covered by the GNU Public License Version 3.
- For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
- You should have received a copy of this license
- along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+/**
+ *    @author Javier Gonzalez
+ *    @author Gonzalo Gonzalez
+ *
+ *    Copyright (C) 2015 ownCloud, Inc.
+ *
+ *    This code is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public
+ License, version 3,
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #import "FileProvider.h"
